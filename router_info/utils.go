dc1b722b332e63ea5d04e5c7577d88fcef8c995a
eyedeekay <hankhill19580@gmail.com>
2025-07-09 16:11:52 -0400

    Re-org: router_info

---
+++ router_info/utils.go
@@ -0,0 +1,19 @@
+package router_info
+
+import (
+	"strconv"
+)
+
+// bytesToString converts a byte slice into a string like [1, 2, 3] -> "1, 2, 3"
+// Moved from: router_info.go
+func bytesToString(bytes []byte) string {
+	str := "["
+	for i, b := range bytes {
+		str += strconv.Itoa(int(b))
+		if i < len(bytes)-1 {
+			str += ", "
+		}
+	}
+	str += "]"
+	return str
+}
