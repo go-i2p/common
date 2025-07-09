a06df8ead5d60954fcf57268e9d79ab7781fbd2a
eyedeekay <hankhill19580@gmail.com>
2025-07-09 15:57:37 -0400

    Re-org: dest

---
+++ destination/utils.go
@@ -0,0 +1,9 @@
+// Package destination implements the I2P Destination common data structure
+package destination
+
+import (
+	"github.com/go-i2p/logger"
+)
+
+// log provides structured logging for the destination package
+var log = logger.GetGoI2PLogger()
