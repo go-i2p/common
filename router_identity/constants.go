27b4ac4984caf32ce6d9cca51618e829437d070a
eyedeekay <hankhill19580@gmail.com>
2025-07-14 19:20:37 -0400

    Refactor: reorganize router_identity.go

---
+++ router_identity/constants.go
@@ -0,0 +1,5 @@
+// Package router_identity implements the I2P RouterIdentity common data structure
+package router_identity
+
+// Currently no constants are defined for this package.
+// This file is created to follow Go code organization best practices.
