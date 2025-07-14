112b52e1e1a58440431fc0112586f97ef331ebc0
eyedeekay <hankhill19580@gmail.com>
2025-07-14 19:57:21 -0400

    Refactor: constants breakout

---
+++ destination/constants.go
@@ -0,0 +1,8 @@
+// Package destination implements the I2P Destination common data structure
+package destination
+
+// ============ I2P Address Generation Constants ============
+
+// I2P_BASE32_SUFFIX is the standard suffix for I2P base32 addresses
+// Used in destination address generation to create valid I2P hostnames
+const I2P_BASE32_SUFFIX = ".b32.i2p"
